4f214873e13fa6370137306f322b348dabdba722
DESBOIS-RENAUDIN Meo <Meo.Desbois-Renaudin@bordeaux-inp.fr>
2023-11-13 17:23:52 +0100

    correction create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -8,18 +8,20 @@ CREATE TABLE Etudiant (
 
 CREATE TABLE Voiture (
     id_voiture SERIAL PRIMARY KEY,
-    FOREIGN KEY id_proprietaire REFERENCES Etudiant(id_etudiant),
+    id_proprietaire INTEGER,
     type_voiture VARCHAR(50),
     couleur VARCHAR(50),
     nb_places_passager INTEGER NOT NULL,
-    etat VARCHAR(50)
+    etat VARCHAR(50),
+    FOREIGN KEY (id_proprietaire) REFERENCES Etudiant(id_etudiant)
 );
 
 CREATE TABLE Trajet (
     id_trajet SERIAL PRIMARY KEY,
-    FOREIGN KEY id_voiture REFERENCES Etudiant(id_voiture),
+    id_voiture INTEGER,
     date_depart DATE NOT NULL,
-    heure_depart TIME NOT NULL
+    heure_depart TIME NOT NULL,
+    FOREIGN KEY (id_voiture) REFERENCES Voiture(id_voiture)
 );
 
 CREATE TABLE Arret (
@@ -30,23 +32,38 @@ CREATE TABLE Arret (
 );
 
 CREATE TABLE Inscription (
-    FOREIGN KEY id_etudiant REFERENCES Etudiant(id_etudiant),
-    FOREIGN KEY id_arret REFERENCES Arret(id_arret),
+    id_etudiant INTEGER,
+    id_arret INTEGER,
+    FOREIGN KEY (id_etudiant) REFERENCES Etudiant(id_etudiant),
+    FOREIGN KEY (id_arret) REFERENCES Arret(id_arret),
     PRIMARY KEY (id_etudiant, id_arret),
     est_valide BOOLEAN DEFAULT TRUE
 );
 
 CREATE TABLE Desservir (
-    FOREIGN KEY id_trajet REFERENCES Trajet(id_trajet),
-    FOREIGN KEY id_arret REFERENCES Arret(id_arret),
-    PRIMARY KEY (id_trajet, id_arret),
+    id_trajet INTEGER,
+    id_arret INTEGER,
+    FOREIGN KEY (id_trajet) REFERENCES Trajet(id_trajet),
+    FOREIGN KEY (id_arret) REFERENCES Arret(id_arret),
+    PRIMARY KEY (id_trajet, id_arret)
 );
 
 CREATE TABLE Evaluer (
-    FOREIGN KEY id_emetteur REFERENCES Etudiant(id_etudiant),
-    FOREIGN KEY id_receveur REFERENCES Etudiant(id_etudiant),
-    FOREIGN KEY id_trajet REFERENCES Trajet(id_trajet),
+    id_emetteur INTEGER,
+    id_receveur INTEGER,
+    id_trajet INTEGER,
+    FOREIGN KEY (id_emetteur) REFERENCES Etudiant(id_etudiant),
+    FOREIGN KEY (id_receveur) REFERENCES Etudiant(id_etudiant),
+    FOREIGN KEY (id_trajet) REFERENCES Trajet(id_trajet),
     PRIMARY KEY (id_emetteur, id_receveur, id_trajet),
     note INTEGER NOT NULL,
     commentaire VARCHAR(500)
 );
+
+ -- DROP TABLE Evaluer;
+ -- DROP TABLE Desservir;
+ -- DROP TABLE Inscription;
+ -- DROP TABLE Arret;
+ -- DROP TABLE Trajet;
+ -- DROP TABLE Voiture;
+ -- DROP TABLE Etudiant;
