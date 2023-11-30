59a96476017e97aedc8eb823ec94eb1567d71098
DESBOIS-RENAUDIN Meo <Meo.Desbois-Renaudin@bordeaux-inp.fr>
2023-11-30 16:33:14 +0100

    nettoyage

--- server/routes/consultation.js
+++ server/routes/consultation.js
@@ -4,23 +4,6 @@ module.exports = app => {
     var router = require("express").Router();
     router.use(require("express").json());
 
-    // Crée un nouvel étudiant
-    // router.post("/", etudiants.create);
-
-    // Renvoie tout les étudiants
-    // router.get("/", etudiants.findAll);
-
-    // Renvoie un étudiant avec un id donné
-    // router.get("/:id", etudiants.findOne);
-
-    // Met à jour un étudiant
-    // router.put("/:id", etudiants.update);
-
-    // Supprime un étudiant avec un id donné
-    // router.delete("/:id", etudiants.delete);
-
-    // NEW
-
     // liste des véhicules disponibles pour un jour donné pour une ville donnée
     router.get("/av/:city", consultation.availableVehicules);
 
