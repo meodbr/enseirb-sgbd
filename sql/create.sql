13ac4551617fdd6186532c5e1524547e053b51fb
DESBOIS-RENAUDIN Meo <Meo.Desbois-Renaudin@bordeaux-inp.fr>
2023-12-03 16:00:22 +0100

    rajout des délétions en cascade et triggers de drop

--- sql/create.sql
+++ sql/create.sql
@@ -23,7 +23,7 @@ CREATE TABLE Voiture (
 
 CREATE TABLE Voyage (
     id_voyage SERIAL PRIMARY KEY,
-    id_voiture INTEGER NOT NULL,
+    id_voiture INTEGER,
     distance INTEGER NOT NULL,
     date_depart DATE NOT NULL,
     heure_depart TIME NOT NULL
@@ -45,29 +45,85 @@ CREATE TABLE Inscription (
 );
 
 CREATE TABLE Evaluation (
-    id_emetteur INTEGER NOT NULL,
-    id_receveur INTEGER NOT NULL,
-    id_voyage INTEGER NOT NULL,
-    CONSTRAINT pk_Evaluation PRIMARY KEY (id_emetteur, id_receveur, id_voyage),
+    id_evaluation SERIAL PRIMARY KEY,
+    id_emetteur INTEGER,
+    id_receveur INTEGER,
+    id_voyage INTEGER,
+    -- CONSTRAINT pk_Evaluation PRIMARY KEY (id_emetteur, id_receveur, id_voyage),
     note INTEGER NOT NULL,
     commentaire VARCHAR(500)
 );
 
 
 ALTER TABLE Voiture
-    ADD CONSTRAINT fk_Voiture FOREIGN KEY (id_proprietaire) REFERENCES Etudiant(id_etudiant);
+    ADD CONSTRAINT fk_Voiture FOREIGN KEY (id_proprietaire) REFERENCES Etudiant(id_etudiant) ON DELETE CASCADE;
 
 ALTER TABLE Voyage
     ADD CONSTRAINT fk_Voyage FOREIGN KEY (id_voiture) REFERENCES Voiture(id_voiture);
 
 ALTER TABLE Arret
-    ADD CONSTRAINT fk_Voiture FOREIGN KEY (id_voyage) REFERENCES Voyage(id_voyage);
+    ADD CONSTRAINT fk_Voiture FOREIGN KEY (id_voyage) REFERENCES Voyage(id_voyage) ON DELETE CASCADE;
 
 ALTER TABLE Inscription
-    ADD CONSTRAINT fk1_Inscription FOREIGN KEY (id_etudiant) REFERENCES Etudiant(id_etudiant),
-    ADD CONSTRAINT fk2_Inscription FOREIGN KEY (id_arret) REFERENCES Arret(id_arret);
+    ADD CONSTRAINT fk1_Inscription FOREIGN KEY (id_etudiant) REFERENCES Etudiant(id_etudiant) ON DELETE CASCADE,
+    ADD CONSTRAINT fk2_Inscription FOREIGN KEY (id_arret) REFERENCES Arret(id_arret) ON DELETE CASCADE;
 
 ALTER TABLE Evaluation
      ADD CONSTRAINT fk1_Evaluation FOREIGN KEY (id_emetteur) REFERENCES Etudiant(id_etudiant),
      ADD CONSTRAINT fk2_Evaluation FOREIGN KEY (id_receveur) REFERENCES Etudiant(id_etudiant),
      ADD CONSTRAINT fk3_Evaluation FOREIGN KEY (id_voyage) REFERENCES Voyage(id_voyage);
+
+
+-- deletion handling
+
+CREATE OR REPLACE FUNCTION update_voyage_on_voiture_delete()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE Voyage
+    SET id_voiture = NULL
+    WHERE id_voiture = OLD.id_voiture;
+
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION update_evaluations_on_student_delete()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE Evaluation
+    SET id_emetteur = NULL
+    WHERE id_emetteur = OLD.id_etudiant;
+
+    UPDATE Evaluation
+    SET id_receveur = NULL
+    WHERE id_receveur = OLD.id_etudiant;
+
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION update_evaluations_on_voyage_delete()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE Evaluation
+    SET id_voyage = 0
+    WHERE id_voyage = OLD.id_voyage;
+
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_voyage_trigger
+BEFORE DELETE ON Voiture
+FOR EACH ROW
+EXECUTE FUNCTION update_voyage_on_voiture_delete();
+
+CREATE TRIGGER update_evaluations_trigger
+BEFORE DELETE ON Etudiant
+FOR EACH ROW
+EXECUTE FUNCTION update_evaluations_on_student_delete();
+
+CREATE TRIGGER update_evaluations_on_voyage_delete_trigger
+BEFORE DELETE ON Voyage
+FOR EACH ROW
+EXECUTE FUNCTION update_evaluations_on_voyage_delete();
