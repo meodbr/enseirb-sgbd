1d12ad2b0818a66701d3c64dbef88142de79966c
Maxime Gajic <Maxime.Gajic@bordeaux-inp.fr>
2023-12-03 18:25:51 +0100

    querystatpassager

--- server/queries/stats.js
+++ server/queries/stats.js
@@ -22,9 +22,20 @@ function queryStatDistance(){
 }
 
 function queryStatPassagers(){
-    return(`SELECT v.date_depart, avg(v.distance) as dist
-    from voyage v
-    group by (v.date_depart)`);
+    return(`SELECT nb_passager/nb_voyage as moyenne_passager 
+    FROM (
+        (SELECT sum(cpt) as nb_passager 
+        from (
+            SELECT v.id_voyage, count(e.id_etudiant) AS cpt
+            FROM Etudiant e NATURAL JOIN inscription i NATURAL JOIN arret a NATURAL JOIN voyage v
+            GROUP BY (v.id_voyage,i.est_valide)
+            Having i.est_valide = 'TRUE'
+            order by (v.id_voyage) ASC
+             )) 
+    CROSS JOIN
+        (SELECT count(v.id_voyage) as nb_voyage
+        from voyage v)
+         )`);
 }
 
 
