1313afb3908daf96ec5e150875f31bce1b6ebbe3
Maxime Gajic <Maxime.Gajic@bordeaux-inp.fr>
2023-12-04 17:13:57 +0100

    distance par jour

--- sql/select.sql
+++ sql/select.sql
@@ -49,19 +49,25 @@ SELECT nb_passager/nb_voyage as moyenne_passager
     );
 
 --moyenne des distances parcourues en covoiturage par jour
+SELECT avg(dist)
+from (
 SELECT v.date_depart, avg(v.distance) as dist
 from voyage v
-group by (v.date_depart);
+group by (v.date_depart)
+)
+;
 
 --classement des meilleurs conducteurs d’aprés les avis
 SELECT e.nom, e.prenom, AVG(ev.note) as avg
 FROM etudiant e JOIN evaluation ev ON e.id_etudiant=ev.id_receveur JOIN voyage vo ON vo.id_voyage = ev.id_voyage JOIN voiture v ON vo.id_voiture = v.id_voiture 
 where ev.id_receveur = v.id_proprietaire
 group by (e.id_etudiant)
-order by (avg) DESC;
+order by (avg) DESC
+LIMIT 5;
 
 --classement des villes selon le nombre de trajets qui les dessert
 SELECT a.ville, count(v.id_voyage) as nombre_arret
 FROM voyage v join  arret a on a.id_voyage = v.id_voyage
 group by (a.ville)
-order by (nombre_arret) DESC;
+order by (nombre_arret) DESC
+LIMIT 5;
