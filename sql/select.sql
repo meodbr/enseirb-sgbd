2aba3da0ee707fd343e03a21e6500f2c31444037
Maxime Gajic <Maxime.Gajic@bordeaux-inp.fr>
2023-12-04 16:42:21 +0100

    del funcion.sql

--- sql/select.sql
+++ sql/select.sql
@@ -29,3 +29,39 @@ FROM Voyage NATURAL JOIN Arret
 WHERE ville = $1
 AND date_depart >= $2
 AND date_depart <= $3;
+
+--STATISTIQUES
+
+--moyenne des passagers sur l’ensemble des trajets effectués
+SELECT nb_passager/nb_voyage as moyenne_passager 
+    FROM (
+        (SELECT sum(cpt) as nb_passager 
+        from (
+            SELECT v.id_voyage, count(e.id_etudiant) AS cpt
+            FROM Etudiant e NATURAL JOIN inscription i NATURAL JOIN arret a NATURAL JOIN voyage v
+            GROUP BY (v.id_voyage,i.est_valide)
+            Having i.est_valide = 'TRUE'
+            order by (v.id_voyage) ASC
+             )) 
+    CROSS JOIN
+        (SELECT count(v.id_voyage) as nb_voyage
+        from voyage v)
+    );
+
+--moyenne des distances parcourues en covoiturage par jour
+SELECT v.date_depart, avg(v.distance) as dist
+from voyage v
+group by (v.date_depart);
+
+--classement des meilleurs conducteurs d’aprés les avis
+SELECT e.nom, e.prenom, AVG(ev.note) as avg
+FROM etudiant e JOIN evaluation ev ON e.id_etudiant=ev.id_receveur JOIN voyage vo ON vo.id_voyage = ev.id_voyage JOIN voiture v ON vo.id_voiture = v.id_voiture 
+where ev.id_receveur = v.id_proprietaire
+group by (e.id_etudiant)
+order by (avg) DESC;
+
+--classement des villes selon le nombre de trajets qui les dessert
+SELECT a.ville, count(v.id_voyage) as nombre_arret
+FROM voyage v join itineraire i on i.id_voyage = v.id_voyage join arret a on a.id_arret = i.id_arret
+group by (a.ville)
+order by (nombre_arret) DESC;
