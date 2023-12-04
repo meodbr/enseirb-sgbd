5a9e844ed91e2f7be4410036bde9387f04982a15
Maxime Gajic <Maxime.Gajic@bordeaux-inp.fr>
2023-12-04 16:58:30 +0100

    classement 5

--- server/queries/stats.js
+++ server/queries/stats.js
@@ -5,14 +5,16 @@ function queryStatConducteur(){
     FROM etudiant e JOIN evaluation ev ON e.id_etudiant=ev.id_receveur join voyage vo on vo.id_voyage = ev.id_voyage join voiture v on vo.id_voiture = v.id_voiture 
     where ev.id_receveur = v.id_proprietaire
     group by (e.id_etudiant)
-    order by (avg) DESC`);
+    order by (avg) DESC
+    LIMIT 5`);
 }
 
 function queryStatVille() {
     return(`SELECT a.ville, count(v.id_voyage) as nombre_arret
     FROM Voyage v NATURAL JOIN Arret a
     group by (a.ville)
-    order by (nombre_arret) DESC`);
+    order by (nombre_arret) DESC
+    LIMIT 5`);
 }
 
 function queryStatDistance(){
